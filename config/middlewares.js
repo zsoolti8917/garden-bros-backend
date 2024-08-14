dc34c5e6364e4f95f89ddce166dbab1075461e8e
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-08-14 22:34:21 +0200

    deleted cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,15 +2,7 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://gardenbros.sk'], // Replace with your frontend domain or IP
-      headers: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    },
-  },
-  
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
